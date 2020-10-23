2c9ac918a18fbed0e381c199aab3f3b9c41a1b67
Will Studabaker <will.studabaker@gmail.com>
2020-10-23 18:05:09 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -112,9 +112,9 @@ def play(board)
   turn(board)
  end
  if won?(board)
-   puts "Congratulations #{current_player(board)}"
+   puts "Congratulations #{winner(board)!}"
  elsif draw?(board)
-   puts "Draw!"
+   puts "Cat's Game!"
  end
 end
 
